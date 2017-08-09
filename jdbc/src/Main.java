419217bcbecd0dd5c094d8a44ea347f34087c3b3
bingzhang <bingzhang_q@live.cn>
2017-08-09 17:19:20 +0800

    账号创建时间自动生成

--- jdbc/src/Main.java
+++ jdbc/src/Main.java
@@ -1,22 +1,13 @@
-
-import java.sql.Connection;
-
 import dao.UsersDAO;
 import dbentity.Users;
-import util.DBHelper;
+import util.SQLTime;
 
 public class Main {
     public static void main(String args[])
     {
-//        Date time = new Date();
-//        System.out.println(time.getTime());
         try {
-            Connection conn1 = DBHelper.getConnection();
-            Connection conn2 = DBHelper.getConnection();
 
-            System.out.println(conn1==conn2);
-
-            Users s = new Users(4,"baron","456",21,"男","北京市朝阳区","2017-08-07 09:22:22");
+            Users s = new Users(5,"小智","451236",23,"男","北京市大兴区",new SQLTime().SQLDateTime());
 
             UsersDAO udao = new UsersDAO();
             if (udao.addStudent(s)){
